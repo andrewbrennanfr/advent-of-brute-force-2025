d0a5cc9905acbad0fa6e8598814874ee1aa74bb5
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2025-12-02 13:42:25 +0100

    :recycle: Refactor day 02

--- src/02/index.ts
+++ src/02/index.ts
@@ -8,44 +8,38 @@ const getIds = (input: string): [number, number][] =>
         .map(([left = dev.error(), right = dev.error()]) => [+left, +right])
 
 const getInvalids = (
-    start: number,
-    end: number,
+    [start, end]: [number, number],
     isInvalid: (string: string) => boolean,
     invalids: number[] = [],
 ): number[] =>
     start > end ? invalids : (
         getInvalids(
-            start + 1,
-            end,
+            [start + 1, end],
             isInvalid,
             isInvalid(`${start}`) ? [...invalids, start] : invalids,
         )
     )
 
-export const part01 = (input: string): number => {
-    const ids = getIds(input)
-    const invalids = ids.flatMap((id) =>
-        getInvalids(
-            ...id,
-            (string) =>
-                math.isDivisible(string.length, 2) &&
-                /^(?<pattern>.+)\k<pattern>$/.test(string),
+export const part01 = (input: string): number =>
+    math.sum(
+        getIds(input).flatMap((id) =>
+            getInvalids(
+                id,
+                (string) =>
+                    math.isDivisible(string.length, 2) &&
+                    /^(?<pattern>.+)\k<pattern>$/.test(string),
+            ),
         ),
     )
 
-    return math.sum(invalids)
-}
-
-export const part02 = (input: string): number => {
-    const ids = getIds(input)
-    const invalids = ids.flatMap((id) =>
-        getInvalids(
-            ...id,
-            (string) =>
-                string.length > 1 &&
-                /^(?<pattern>.+)\k<pattern>+$/.test(string),
+export const part02 = (input: string): number =>
+    math.sum(
+        getIds(input).flatMap((id) =>
+            getInvalids(
+                id,
+                (string) =>
+                    string.length > 1 &&
+                    /^(?<pattern>.+)\k<pattern>+$/.test(string),
+            ),
         ),
     )
-
-    return math.sum(invalids)
-}
